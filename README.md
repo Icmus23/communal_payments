fe0bd4e033a3e32a7cd05228d25c54d0f6ce6d7c
Icmus23 <i.cmus@mail.ru>
2017-02-23 12:23:10 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+communal_payments
+=================
+
+A Symfony project created on February 23, 2017, 11:49 am.
---
+++ src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class DefaultController extends Controller
+{
+    const HOT_WATER_RATE            = 83.1; // куб гарячей воды
+    const COLD_WATER_RATE           = 6.84; // куб холодной воды
+    const WATER_OUTFALL             = 6.93; // водоотвод
+    const ELECTRICITY_LESS_THAN_100 = 0.714; // электричество до 100 кВТ
+    const ELECTRICITY_MORE_THAN_100 = 1.29; // электричество больше 100 кВТ
+    const FLAT_RATE                 = 110.54; // содержание придворовых территорий
+    const INTERCOM_RATE             = 15.60; // домофон
+    const HEATING_RATE              = 1182.31; // отопление
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction(Request $request)
+    {
+        $hotWaterAmount = $this->calculateHotWaterAmount(7);
+        $coldWaterAmount = $this->calculateColdWaterAmount(3);
+        $electricityAmount = $this->calculateElectricityAmount(131);
+        $sum =
+            $hotWaterAmount +
+            $coldWaterAmount +
+            $electricityAmount +
+            self::FLAT_RATE +
+            self::INTERCOM_RATE +
+            self::HEATING_RATE;
+
+        return $this->render('default/index.html.twig', [
+            'hotWaterAmount' => $hotWaterAmount,
+            'coldWaterAmount' => $coldWaterAmount,
+            'electricityAmount' => $electricityAmount,
+            'flatRate' => self::FLAT_RATE,
+            'intercomRate' => self::INTERCOM_RATE,
+            'heatingRate' => self::HEATING_RATE,
+            'sum' => $sum
+        ]);
+    }
+
+    private function calculateHotWaterAmount($cubicMeters)
+    {
+        return $cubicMeters * (self::HOT_WATER_RATE + self::WATER_OUTFALL);
+    }
+
+    private function calculateColdWaterAmount($cubicMeters)
+    {
+        return $cubicMeters * (self::COLD_WATER_RATE + self::WATER_OUTFALL);
+    }
+
+    private function calculateElectricityAmount($kilowatts)
+    {
+        $hundred = 100;
+
+        if ($kilowatts > $hundred) {
+            return ($hundred * self::ELECTRICITY_LESS_THAN_100) + ($kilowatts - $hundred) * self::ELECTRICITY_MORE_THAN_100;
+        } else {
+            return $kilowatts * self::ELECTRICITY_LESS_THAN_100;
+        }
+    }
+}
